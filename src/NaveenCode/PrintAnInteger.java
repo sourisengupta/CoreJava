2aa843cc40330409af6fbdb5212bc048f97252ac
sourisengupta <sengupta.souri@gmail.com>
2022-01-28 20:38:31 +0530

    first program

---
+++ src/NaveenCode/PrintAnInteger.java
@@ -0,0 +1,15 @@
+package NaveenCode;
+
+import java.util.Scanner;
+
+public class PrintAnInteger {
+	public static void main(String args[]) {
+		
+		Scanner reader = new Scanner(System.in);		
+		System.out.println("Enter a number: ");
+		
+		int num = reader.nextInt();
+		System.out.println(num);
+		
+	}
+}
